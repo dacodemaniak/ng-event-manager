a8130728d78edd77eed1fefe7f01923546c68984
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-12-04 13:30:06 +0100

    Update fakebackend

--- src/app/shared/services/fake-backend.service.ts
+++ src/app/shared/services/fake-backend.service.ts
@@ -3,16 +3,23 @@ import { Injectable } from '@angular/core';
 import {
   HttpEvent,
   HttpHandler,
-  HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS
+  HttpInterceptor, HttpRequest, HttpResponse, HTTP_INTERCEPTORS
 } from '@angular/common/http'
 
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, dematerialize, materialize, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class FakeBackendService implements HttpInterceptor {
 
   constructor() { }
 
+  /**
+   * intercept
+   * @param request HttpRequest
+   * @param next  HttpHandler
+   * Intercepte une requête HTTP
+   */
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
@@ -20,7 +27,57 @@ export class FakeBackendService implements HttpInterceptor {
     const { url, method, headers, body } = request
 
     console.log(`Request ${url} was intercepted`)
-    return next.handle(request)
+
+    return of(null)
+      .pipe(
+        mergeMap(handleRoute)
+      ).pipe(
+        materialize()
+      ).pipe(
+        delay(500)
+      ).pipe(
+        dematerialize()
+      );
+
+    function handleRoute(): Observable<HttpEvent<any>> {
+      const userRegex: RegExp = /\/api\/v2\/user+$/;
+      switch (true) {
+        case userRegex.test(url) && method === 'POST':
+          return registerUser(request)
+        default:
+          return next.handle(request);
+      }
+
+      // route functions
+
+      function registerUser(request: HttpRequest<any>): Observable<HttpResponse<any>> {
+        return ok(request.body);
+      }
+
+      // helper functions
+
+      function ok(body?: any): Observable<HttpResponse<any>> {
+        return of(new HttpResponse({ status: 200, body }));
+      }
+
+      function error(message): Observable<never> {
+        return throwError({ error: { message } });
+      }
+
+      function unauthorized(): Observable<never> {
+        return throwError({ status: 401, error: { message: 'Unauthorised' } });
+      }
+
+      function isLoggedIn(): boolean {
+        return headers.get('Authorization') === 'Bearer fake-jwt-token';
+      }
+
+      function idFromUrl(): number {
+        const urlParts = url.split('/');
+        return +urlParts[urlParts.length - 1];
+      }
+
+    }
   }
 }
 
